060ee4ac3f84d2c89ea8e03b9689facc810f0529
Kooshan <55038456+akay4ty7@users.noreply.github.com>
2019-10-28 16:20:05 +1300

    Camera Following Player
    methods include
    
    Interlerping
    (chosen) Smooth Dampening

---
+++ Mr Jupiter/Assets/CameraFollow2D.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    float timeOffset;
+
+    [SerializeField]
+    Vector2 posOffset;
+
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //camera start position
+        Vector3 startPos = transform.position;
+        //players current postion
+        Vector3 endPos = player.transform.position;
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+        //lerping camera follow
+        //transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+
+        transform.position = Vector3.SmoothDamp(startPos, endPos, ref velocity, timeOffset);
+
+        //shitty camera follow
+        //transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+
+    }
+}
